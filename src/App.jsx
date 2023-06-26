769ac0065c5322a56f6f5f80caf7974b137e3394
Daniel Dumitru <daniel@iviteb.com>
2023-06-26 16:09:01 +0300

    iPhone 3D

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useRef } from 'react'
+
+import Nav from './components/Nav'
+import Jumbotron from './components/Jumbotron'
+import SoundSection from './components/SoundSection'
+import DisplaySection from './components/DisplaySection'
+import WebgiViewer from './components/WebgiViewer'
+import Loader from './components/Loader'
 
 function App() {
+  const webgiViewerRef = useRef()
+  const contentRef = useRef()
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview()
+  }
 
   return (
-    <div className="App">
-      Hello
+    <div className='App'>
+      <Loader />
+      <div ref={contentRef} id='content'>
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
